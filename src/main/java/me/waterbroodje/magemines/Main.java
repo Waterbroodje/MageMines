dc74a255db4bce53bac65e22fc3620ad812e2daf
Waterbroodje <wortelschaaf.info@gmail.com>
2022-01-07 14:42:02 +0100

    Added config.yml

---
+++ src/main/java/me/waterbroodje/magemines/Main.java
@@ -0,0 +1,11 @@
+package me.waterbroodje.magemines;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+}
