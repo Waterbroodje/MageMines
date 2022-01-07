d5cfccd5b4e3d88152923cd627e832e7c794bd8c
Waterbroodje <wortelschaaf.info@gmail.com>
2022-01-07 15:01:54 +0100

    Added runnable and time

--- src/main/java/me/waterbroodje/magemines/Main.java
+++ src/main/java/me/waterbroodje/magemines/Main.java
@@ -1,5 +1,8 @@
 package me.waterbroodje.magemines;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.block.Block;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
@@ -20,7 +23,12 @@ public final class Main extends JavaPlugin {
 
                 if ((now.getHours() + ":" + now.getMinutes()).equalsIgnoreCase(getConfig().getString("reset-time"))) {
                     getConfig().getConfigurationSection("mines").getKeys(false).forEach(key -> {
+                        Location loc1 = new Location(Bukkit.getWorld(getConfig().getString("mines." + key + ".location-1.world")), getConfig().getInt("mines." + key + ".location-1.x"), getConfig().getInt("mines." + key + ".location-1.y"), getConfig().getInt("mines." + key + ".location-1.z"));
+                        Location loc2 = new Location(Bukkit.getWorld(getConfig().getString("mines." + key + ".location-2.world")), getConfig().getInt("mines." + key + ".location-2.x"), getConfig().getInt("mines." + key + ".location-2.y"), getConfig().getInt("mines." + key + ".location-2.z"));
+                        Cuboid cuboid = new Cuboid(loc1, loc2);
+                        cuboid.getBlocks().forEach(block -> {
 
+                        });
                     });
                 }
             }
