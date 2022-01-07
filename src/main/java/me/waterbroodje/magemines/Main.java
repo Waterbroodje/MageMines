acd1084fc96b8a8b1def3630d9d163ec55426463
Waterbroodje <wortelschaaf.info@gmail.com>
2022-01-07 14:56:05 +0100

    Added Cuboid class to get all the blocks in 2 locations.

--- src/main/java/me/waterbroodje/magemines/Main.java
+++ src/main/java/me/waterbroodje/magemines/Main.java
@@ -1,13 +1,29 @@
 package me.waterbroodje.magemines;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.Date;
+import java.util.TimeZone;
 
 public final class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
         this.saveDefaultConfig();
+        TimeZone.setDefault( TimeZone.getTimeZone("GMT+1"));
 
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                Date now = new Date();
 
+                if ((now.getHours() + ":" + now.getMinutes()).equalsIgnoreCase(getConfig().getString("reset-time"))) {
+                    getConfig().getConfigurationSection("mines").getKeys(false).forEach(key -> {
+
+                    });
+                }
+            }
+        }.runTaskTimer(this, 0L, 1200L);
     }
 }
